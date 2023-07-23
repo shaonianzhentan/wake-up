7e6f0c781b060cdc39cd295325b0d13a2576e948
shaonianzhetnan <5294022+shaonianzhentan@users.noreply.github.com>
2023-07-23 22:50:01 +0800

    自定义语音唤醒

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # wake-up
 专注语音唤醒
 
+安装依赖
+```bash
+pip3 install -r requirements.txt
+```
 
-
+运行
+```bash
+python3 wake.py
+```
 
 自定义唤醒词
-https://snowboy.hahack.com/
+- https://snowboy.hahack.com/
 
-
-核心代码来自
+核心代码来自于`wukong-robot`
 - https://wukong.hahack.com/
---
+++ wake.py
@@ -0,0 +1,37 @@
+import signal
+from snowboy import snowboydecoder
+
+class WakeUp():
+  
+    def __init__(self):
+        self.interrupted = False
+        signal.signal(signal.SIGINT, self.signal_handler) # 捕获ctrl+c
+
+        model = '萝卜头.pmdl'
+        self.detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5) # 设置语音模型与敏感度
+        print('Listening... Press Ctrl+Z to exit')
+        
+    def signal_handler(self, signal, frame):
+        self.interrupted = True
+
+    def interrupt_callback(self):
+        return self.interrupted
+
+    def detected_callback(self, is_snowboy):
+        print('叮')
+
+    def audio_recorder_callback(self, fp, callback=None):
+        print(fp)
+        print('咚')
+
+    # 启动机器人
+    def run(self):
+        self.detector.start(
+              detected_callback=self.detected_callback, 
+              audio_recorder_callback=self.audio_recorder_callback,
+              interrupt_check=self.interrupt_callback,
+              silent_count_threshold=15,
+              recording_timeout=5 * 4,
+              sleep_time=0.03)
+
+WakeUp().run()
